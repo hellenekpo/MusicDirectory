ec0d12fde7a1c7aae28f96706ceaa39250176eee
Hellen Ekpo <he560@nyu.edu>
2020-06-02 03:35:39 -0500

    latest commit

--- MusicDirectory/Playlist.h
+++ MusicDirectory/Playlist.h
@@ -12,7 +12,7 @@ public:
     Playlist(const std::string& title, const std::string& genre);
     bool removeSong(const Song& song);
     bool addSong(Song& song);
-    virtual const std::string& getTitle() const = 0;
+    const std::string& getTitle() const;
     size_t lengthOfCollection() const;
     void listSongs() const;
     void playSong(int trackNumber) const;
--- MusicDirectory/main.cpp
+++ MusicDirectory/main.cpp
@@ -22,16 +22,16 @@ void commands(vector<Person*>& people, vector<Song*>& songs, vector<Album*>& alb
               vector<RecordLabel*>& recordLabels, vector<Playlist*>& playlist) {
     bool stop = false;
     while (!stop) {
-        cout << "Welcome to this Music Program! Enter 'c' to print commands\n";
+        cout << "Welcome to this Music Directory Program! Enter 'c' to print commands\n";
         string command;
         cin >> command;
         if (command == "c") {
-            cout << "To make a new Fan, please enter 'f' following the name and"
-            << "their introdction.\n; To make a new Artist please enter 'ar'"
+            cout << "To make a new Fan, please enter 'f' following the name and "
+            << "their introduction.\nTo make a new Artist please enter 'art' "
             << "following thier name, go to introduction, and adlib.\nTo make"
-            << "a new Album please enter 'al' following the title of the album"
-            << "and the year it came out.\nTo mske a new Record Label, please enter"
-            << "'rl' following the name and the year founded.\nTo make a new Song, please"
+            << "a new Album please enter 'al' following the title of the album "
+            << "and the year it came out.\nTo mske a new Record Label, please enter "
+            << "'rl' following the name and the year founded.\nTo make a new Song, please "
             << "enter 's' following the name of the song.\nTo make a new Playlist, please "
             << "enter 'pl' following the name of the playlist, and the genre.\nTo exit, please "
             << "enter 'exit'\n";
@@ -50,15 +50,44 @@ void commands(vector<Person*>& people, vector<Song*>& songs, vector<Album*>& alb
                 if (commandnext == "al") {
                     Album* newAlbum =  new Album(title, num);
                     albums.push_back(newAlbum);
+                    cout << "New Album: " << title << endl << "Released: "
+                    << num << "\nsuccessfullly added to the directory.\n";
                 }
                 else {
                     RecordLabel* newRL = new RecordLabel(title, num);
                     recordLabels.push_back(newRL);
+                    cout << "New Record Label: " << title << endl << "Founded: "
+                    << num << "\nsuccessfully added to the directory.\n";
                 }
             }
             else {
-                if (commandnext == "ar") {
-                    cin >> info1 >> info2;
+                if (commandnext == "s") {
+                    Song* newSong = new Song(title);
+                    songs.push_back(newSong);
+                    cout << "New Song: " << title << "\nsuccessfully added to the directoy.\n";
+                }
+                else {
+                    if (commandnext == "f" || commandnext == "pl" || commandnext == "art") {
+                        cin >> info1;
+                        if (commandnext == "f") {
+                            Fan* newFan = new Fan(title, info1);
+                            people.push_back(newFan);
+                            cout << "New Fan: " << title << "\nsuccessfully added to the directory.\n";
+                        }
+                        else if (commandnext == "pl") {
+                            Playlist* newPlay = new Playlist(title, info1);
+                            playlist.push_back(newPlay);
+                            cout << "New Playlist: " << title << endl << "Genre: "
+                            << info1 << "\nsuccessfully added to the directory.\n";
+            
+                        }
+                        else if (commandnext == "art") {
+                            cin >> info2;
+                            Artist* newArt = new Artist(title, info1, info2);
+                            people.push_back(newArt);
+                            cout << "New Artist: " << title << "\nsuccessfully added to the directory.\n";
+                        }
+                    }
                 }
             }
         }
